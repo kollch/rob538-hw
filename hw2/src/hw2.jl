eacc64250700b99dc669a4cdf9b7046841615a71
Charles Koll <charles@kollonline.com>
2022-10-10 21:35:57 -0700

    Outline approach and remove Flux dependency

--- hw2/src/hw2.jl
+++ hw2/src/hw2.jl
@@ -1,6 +1,20 @@
 module hw2
 
-using Flux
+# y  1 2 3 4 5 6 7 8 9 10 x
+# 1 |d|l|l|l|d|l|l|l|l|l|
+# 2 |d|r|d|u|d|r|d|r|d|u|
+# 3 |d|u|d|u|r|u|r|u|d|u|
+# 4 |d|u|d|u|l|l|l|l|l|u|
+# 5 |r|u|r|r|r|r|r|r|r|u|
+# start: (4, 3)  (x, y)
+# target: (2, 4)
+
+# Calculate value function of each state for entire policy
+# Iterate:
+#     Follow eps-greedy for next action (top 2)
+#     Recompute value of current state based on next action
+#       (since might not take best action, set as max of curr and new)
+#     Take action
 
 greet() = print("Hello World!")
 
